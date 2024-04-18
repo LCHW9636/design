1586767384e822dd55ec7470cd953cd1d1d49284
LCHW9636 <157003738+LCHW9636@users.noreply.github.com>
2024-04-18 18:00:27 +0900

    ..
    ..

--- police/js/main.js
+++ police/js/main.js
@@ -35,19 +35,23 @@ $(document).ready(function(){
 
     const service_swiper = new Swiper('.service .swiper', { /* 팝업을 감싼는 요소의 class명 */
 	slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-	spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+	spaceBetween: 12, /* 팝업과 팝업 사이 여백 */
 	breakpoints: {
+		320: {    /* 640px 이상일때 적용 */
+			slidesPerView: 2,
+			spaceBetween: 12,
+		},
 		640: {    /* 640px 이상일때 적용 */
-			slidesPerView: 3,
-			spaceBetween: 16,
+			slidesPerView: 4,
+			spaceBetween: 15,
 		},
 		768: {    /* 768px 이상일때 적용 */
-			slidesPerView: 4,
+			slidesPerView: 6,
 			spaceBetween: 20,
 		},
 		1024: {   /* 1024px 이상일때 적용 */
-			slidesPerView: 5,
-			spaceBetween: 30,
+			slidesPerView: 8,
+			spaceBetween: 40,
 		},
 		1280: {    /* 1280px 이상일때 적용 */
 			slidesPerView: 8,
@@ -67,6 +71,49 @@ $(document).ready(function(){
 	},
 });
 
-  
+		const commu_swiper = new Swiper('.commu .swiper', { /* 팝업을 감싼는 요소의 class명 */
+		slidesPerView: 4, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+		breakpoints: {
+			640: {    /* 640px 이상일때 적용 */
+				slidesPerView: 4,
+				spaceBetween: 19,
+			},
+			768: {    /* 768px 이상일때 적용 */
+				slidesPerView: 4,
+				spaceBetween: 19,
+			},
+			1024: {   /* 1024px 이상일때 적용 */
+				slidesPerView: 3,
+				spaceBetween: 19,
+			},
+			1280: {    /* 1280px 이상일때 적용 */
+				slidesPerView: 3,
+				spaceBetween: 19,
+			},
+		},
+		centeredSlides: false, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+		loop: false,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+		autoplay: {  /* 팝업 자동 실행 */
+			delay: 250000000,
+			disableOnInteraction: false,
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+
+		});
+
+		$('.shortcut .family_site .open').on('click', function(){
+			$('.shortcut .family_site').addClass('on')
+		})
+		$('.shortcut .family_site .close').on('click', function(){
+			$('.shortcut .family_site').removeClass('on')
+		})
+
+
+
+
 }) //$(document).ready(function)
 
